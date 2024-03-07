7bc39d0ebd4eb760900d52daaf54cb8328e09ae6
dan ruxton <dan@tantalus-troupe.com>
2024-03-07 10:19:31 +0100

    project updates

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,9 @@ module.exports = {
   testMatch: ["**/tests/**/*.test.+(ts|js)", "**/test/**/*.test.+(ts|js)"],
   coverageReporters: ["lcov", "text-summary", "json-summary"],
   moduleFileExtensions: ["js", "ts"],
+  projects: [
+    "<rootDir>/service-1",
+    "<rootDir>/service-2",
+    "<rootDir>/service-3",
+  ],
 };
